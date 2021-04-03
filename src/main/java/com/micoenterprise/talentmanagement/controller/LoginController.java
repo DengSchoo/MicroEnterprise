182099a6ec5aa6ae7ae4523d093cf217f148ae96
zqing2018112706 <1745884838@qq.com>
2021-04-03 16:57:16 +0800

    update

--- src/main/java/com/micoenterprise/talentmanagement/controller/LoginController.java
+++ src/main/java/com/micoenterprise/talentmanagement/controller/LoginController.java
@@ -1,12 +1,24 @@
 package com.micoenterprise.talentmanagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(HttpServletRequest request){
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        System.out.println(email);
+        return "redirect:/";
     }
 }
--- src/test/java/com/micoenterprise/talentmanagement/TalentmanagementApplicationTests.java
+++ src/test/java/com/micoenterprise/talentmanagement/TalentmanagementApplicationTests.java
@@ -1,13 +1,25 @@
 package com.micoenterprise.talentmanagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.sql.SQLException;
 
 @SpringBootTest
 class TalentmanagementApplicationTests {
+    @Autowired
+    DataSource dataSource;
 
     @Test
     void contextLoads() {
+        try {
+            System.out.println(dataSource.getConnection());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
 }
